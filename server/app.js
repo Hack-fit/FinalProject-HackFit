0795e50747d3e6c97c96981e537bd7812eac6451
Farrel Kardia <fmkardia@gmail.com>
2024-08-07 17:03:59 +0700

    feat:add middleware

--- server/app.js
+++ server/app.js
@@ -4,18 +4,24 @@ if(process.env.NODE_ENV !== "production"){
 
 const express = require("express")
 const UserController = require("./Controller/UserController")
+const authentication = require("./middleware/authentication")
 const app = express()
 const port = 4000
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
  
-app.post('/openai',UserController.openAi)
 
 app.get('/user',UserController.getall)
 app.post('/register',UserController.register)
 app.post('/register-pt',UserController.registerPt)
 app.post('/login',UserController.login)
+
+app.use(authentication)// authentication setelah login, belum handle di client. manual buat headersnya 
+
+app.post('/openai',UserController.openAi)
+app.get('/profile',UserController.finduserbyId)
+
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
